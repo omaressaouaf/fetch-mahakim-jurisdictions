0a341d8ef3791cccc5ff40da7a62d5b8a0ec1bce
Omar-Ess <souafomar@gmail.com>
2022-07-26 21:24:57 +0100

    s

---
+++ index.js
@@ -0,0 +1,42 @@
+const {
+  fetchParentJurisdictions,
+  fetchChildJurisdictionsByInstance,
+  fetchChildJurisdictionsByParent,
+  makeJurisdictionFromMahakimData,
+  writeJurisdictionsIntoJsonFile,
+} = require("./utils");
+
+(async function () {
+  try {
+    var jurisdictions = [];
+
+    for (const jurisdictionType of ["CA", "TPI", "CAC", "TC", "CAA", "TA"]) {
+      console.info(`Fetching jurisdictions of type ${jurisdictionType} ...`);
+
+      const mahakimJurisdictions = await fetchParentJurisdictions(jurisdictionType);
+
+      for (const mahakimJurisdiction of mahakimJurisdictions) {
+        // if it's none appeal court whe need to fetch child insteadof parents jurisdictions
+        if (["TPI", "TC", "TA"].includes(jurisdictionType)) {
+          let mahakimChildJurisdictions = await fetchChildJurisdictionsByInstance(mahakimJurisdiction["idJuridiction"]);
+          for (const mahakimChildJurisdiction of mahakimChildJurisdictions) {
+            jurisdictions.push(await makeJurisdictionFromMahakimData(mahakimChildJurisdiction, jurisdictionType));
+          }
+
+          mahakimChildJurisdictions = await fetchChildJurisdictionsByParent(mahakimJurisdiction["idJuridiction"]);
+          for (const mahakimChildJurisdiction of mahakimChildJurisdictions) {
+            if (!jurisdictions.map(jur => jur.id).includes(mahakimChildJurisdiction["idJuridiction"])) {
+              jurisdictions.push(await makeJurisdictionFromMahakimData(mahakimChildJurisdiction, jurisdictionType));
+            }
+          }
+        } else {
+          jurisdictions.push(await makeJurisdictionFromMahakimData(mahakimJurisdiction, jurisdictionType));
+        }
+      }
+    }
+
+    writeJurisdictionsIntoJsonFile(jurisdictions);
+  } catch (err) {
+    console.log(err.response.data["Message"] || err);
+  }
+})();
---
+++ utils.js
@@ -0,0 +1,68 @@
+var fs = require("fs");
+var axios = require("axios");
+const translate = require("@vitalets/google-translate-api");
+
+axios.defaults.baseURL = "https://www.mahakim.ma/Ar/Services/SuiviAffaires_new/JFunctions/fn.aspx";
+
+const capitalize = text => {
+  return text.charAt(0).toUpperCase() + text.slice(1).toLowerCase();
+};
+
+const translateJurisdictionName = async arabicName => {
+  const res = await translate(arabicName, { to: "fr" }).catch(err => {
+    console.error(err);
+  });
+
+  return capitalize(res.text);
+};
+
+exports.makeJurisdictionFromMahakimData = async (mahakimJurisdiction, type) => {
+  const arabicName = mahakimJurisdiction["nomJuridiction"].trim();
+  const name = await translateJurisdictionName(arabicName);
+
+  return {
+    id: mahakimJurisdiction["idJuridiction"],
+    type,
+    name,
+    arabicName,
+  };
+};
+
+exports.fetchParentJurisdictions = async jurisdictionTypeCode => {
+  const res = await axios.post("/getCA", { typeJuridiction: jurisdictionTypeCode });
+
+  return res.data["d"];
+};
+
+exports.fetchChildJurisdictionsByInstance = async secondJurisdictionInstanceId => {
+  const res = await axios.post("/getJuridiction1instance", {
+    IdJuridiction2Instance: secondJurisdictionInstanceId,
+  });
+
+  return res.data["d"];
+};
+
+exports.fetchChildJurisdictionsByParent = async parentJurisdictionId => {
+  const res = await axios.post("/GetListJuridByIdJuridParent", {
+    IdJuridictionParent: parentJurisdictionId,
+  });
+
+  return res.data["d"];
+};
+
+exports.writeJurisdictionsIntoJsonFile = jurisdictions => {
+  fs.readFile("jurisdictions.json", (err, data) => {
+    if (err) console.error("Error reading the jurisdictions file");
+
+    let jurisdictionsList = JSON.parse(data);
+    jurisdictionsList = jurisdictions;
+
+    console.info("Writing to jurisdictions file ...");
+
+    fs.writeFile("jurisdictions.json", JSON.stringify(jurisdictionsList), err => {
+      if (err) console.error("Error Writing to jurisdictionTypes");
+
+      console.info(`Jurisdictions were written into the file successfully`);
+    });
+  });
+};
